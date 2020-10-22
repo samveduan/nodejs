786541cf346b1f20f531c549122cc3f3aefde081
samve <samveduan@163.com>
2020-10-22 15:43:21 +0800

    http模块

---
+++ http.js
@@ -0,0 +1,35 @@
+var http = require("http")
+var fs = require("fs")
+
+var server = http.createServer();
+server.on("request", function (req, res) {
+    // HTTP Content-type常用对照表：https://tool.oschina.net/commons
+    
+    console.log("当前路径：" + req.url);
+    
+    if (req.url === '/html') {
+        fs.readFile("./file/index.html", function (error, data) {
+            if (!error) {
+                res.setHeader("Content-Type", "text/html; charset=utf-8");
+                res.end(data);
+            } else {
+                res.setHeader("Content-Type", "text/plain; charset=utf-8");
+                res.end("读取html失败！");
+            }
+        })
+    }else if(req.url === "/img"){
+        fs.readFile("./file/sea.jpg", function (error, data) {
+            if (!error) {
+                res.setHeader("Content-Type", "image/jpeg");
+                res.end(data);
+            } else {
+                res.setHeader("Content-Type", "text/plain; charset=utf-8");
+                res.end("读取图片失败！");
+            }
+        })
+    }
+})
+
+server.listen(5555, function () {
+    console.log("服务启动成功......");
+})
